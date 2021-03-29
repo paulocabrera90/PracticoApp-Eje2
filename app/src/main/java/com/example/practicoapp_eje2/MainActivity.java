4b6341cdd1a405de9c9af29a028a47d889927b55
Paulo Cabrera <pauloc@unitech.com.ar>
2021-03-28 22:03:01 -0300

    Ejercicio

---
+++ app/src/main/java/com/example/practicoapp_eje2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.practicoapp_eje2;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void acceder(){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&  checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},1000 );
+            }
+
+            Uri llamadas = Uri.parse("content://call_log/calls");
+        ContentResolver contenedor = getContentResolver();
+        Cursor cursor = contenedor.query(llamadas,null,null,null);
+
+        String nro=null;
+        String tiempo=null;
+        String nombre = null;
+        String apellido = null;
+        String mensaje = null;
+
+        if(cursor.getCount()>0){
+            while(cursor.moveToNext()){
+
+                nro= cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
+                tiempo= cursor.getString(cursor.getColumnIndex(CallLog.Calls.DATE));
+          //      nombre= cursor.getString(cursor.getColumnIndex(Contacts.));
+                apellido= cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NUMBER_LABEL));
+                mensaje= cursor.getString(cursor.getColumnIndex(CallLog.Calls.FEATURES));
+
+                Log.d("nro ", nro);
+                Log.d("tiempo ", tiempo);
+                Log.d("apellido ", apellido);
+                Log.d("mensaje ", mensaje);
+
+            }
+        }
+    }
+}
